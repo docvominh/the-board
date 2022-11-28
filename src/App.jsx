7a14076d9cdcdcb4de0b8bdd0f1e6a9c2c59b406
Minh Pham <minh.phamduc@axonactive.com>
2022-11-28 20:49:26 +0700

    add source

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Stock from './components/stock/Stock'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <section>
+      <Stock />
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/stock/Stock.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Stock.css';
+
+
+class Stock extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Twitter</p>
+            </div>
+        )
+    }
+}
+
+export  default Stock;
