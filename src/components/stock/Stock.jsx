cbac24f29de34e1050bdbc79ccfd1c089bc5b2a4
Minh <phamducminh1990@gmail.com>
2022-11-30 17:03:46 +0700

    update

--- src/components/stock/Stock.jsx
+++ src/components/stock/Stock.jsx
@@ -3,10 +3,32 @@ import './Stock.css';
 
 
 class Stock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars-long-term?ticker=GEX&type=stock&resolution=D&from=1608045870&to=' + Date.now(),
+            {
+                method: 'GET',
+                // mode: "no-cors"
+            })
+            .then(response => {return response})
+            .then(data => {console.log(data)})
+            .catch((error) => {
+                // console.error( error);
+            });
+
+
+    }
+
     render() {
         return (
             <div>
-                <p>Twitter</p>
+                <p>Stock</p>
             </div>
         )
     }
